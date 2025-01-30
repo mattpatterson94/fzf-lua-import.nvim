fa7d58a6f155f7cbb04a3c36735578f20c134d7f
Matt Patterson <mattpatterson@canva.com>
2025-01-30 22:46:19 +1000

    dont paste in line already in buffer and more cleanups

--- lua/actions.lua
+++ lua/actions.lua
@@ -5,8 +5,14 @@ local M = {}
 -- Places the import statement at the top of the file
 -- Adds to the clipboard for additional usage
 M.add_to_buffer = function(selected)
-  utils.copy_to_register(selected[1])
-  utils.paste_in_buffer(selected[1])
+  local line_to_add = selected[1]
+
+  if not utils.line_exists_in_buffer(line_to_add) then
+    utils.paste_in_buffer(line_to_add)
+    utils.copy_to_register(line_to_add)
+  else
+    vim.notify("Import already in buffer. Skipping.")
+  end
 end
 
 return M
--- lua/utils.lua
+++ lua/utils.lua
@@ -21,68 +21,15 @@ M.get_filetype = function()
   return vim.bo[bufnr].filetype
 end
 
-M.remove_duplicates = function(inputTable)
-  local uniqueTable = {}
-  local resultTable = {}
-
-  for _, value in ipairs(inputTable) do
-    if not uniqueTable[value] then
-      uniqueTable[value] = true
-      table.insert(resultTable, value)
+M.line_exists_in_buffer = function(line, buffer)
+  local current_buffer = 0
+  local lines = vim.api.nvim_buf_get_lines(buffer or current_buffer, 0, -1, false)
+  for _, l in ipairs(lines) do
+    if l == line then
+      return true
     end
   end
-
-  return resultTable
-end
-
-M.remove_entries = function(source_table, table_to_remove)
-  local lookup = {}
-  for _, value in ipairs(table_to_remove) do
-    lookup[value] = true
-  end
-  local new_table = {}
-  for _, value in ipairs(source_table) do
-    if not lookup[value] then
-      table.insert(new_table, value)
-    end
-  end
-  return new_table
-end
-
-M.sort_by_frequency = function(inputTable)
-  local frequencies = {}
-
-  -- Count the frequencies of elements in the input table
-  for _, value in ipairs(inputTable) do
-    frequencies[value] = (frequencies[value] or 0) + 1
-  end
-
-  -- Create a table with pairs of elements and their frequencies
-  local elementsAndFrequencies = {}
-  for element, frequency in pairs(frequencies) do
-    table.insert(elementsAndFrequencies, { element = element, frequency = frequency })
-  end
-
-  -- Sort the table based on frequencies in descending order
-  table.sort(elementsAndFrequencies, function(a, b)
-    return a.frequency > b.frequency
-  end)
-
-  local sortedTable = {}
-  for _, pair in ipairs(elementsAndFrequencies) do
-    for _ = 1, pair.frequency do
-      table.insert(sortedTable, pair.element)
-    end
-  end
-
-  return sortedTable
-end
-
-M.concat_tables = function(t1, t2)
-  for i = 1, #t2 do
-    t1[#t1 + 1] = t2[i]
-  end
-  return t1
+  return false
 end
 
 return M
