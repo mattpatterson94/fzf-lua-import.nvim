f728b6b8e5dc53da064b615f0e6b95a1b627952e
Matt Patterson <mattpatterson@canva.com>
2025-01-30 15:14:07 +1000

    support keys in config

--- lua/fzf-lua-import.lua
+++ lua/fzf-lua-import.lua
@@ -1,6 +1,5 @@
-local rg = require("rg")
+local commands = require("commands")
 local utils = require("utils")
-local actions = require("actions")
 
 local M = {
   config = {
@@ -10,35 +9,54 @@ local M = {
     previewer = false,
     multiprocess = true,
     hidden = false,
-    prompt = "Imports> ",
+    keys = {
+      { key = "<leader>ci", mode = { "n" }, command = "live_grep", enabled = true },
+      { key = "<leader>cI", mode = { "n" }, command = "grep_cword", enabled = true },
+    },
   },
 }
 
-M.live_grep = function(opts)
-  local fzf_lua = require("fzf-lua")
-  opts = opts or M.config
-  opts.prompt = "Imports> "
-  opts.actions = {
-    ["default"] = actions.add_to_buffer,
-  }
+M.import = function(opts)
+  local command = commands.commands[opts.args]
+  local filetype = utils.get_filetype()
 
-  return fzf_lua.fzf_live(function(q)
-    local parsed_q = utils.parse_query(q)
-    return rg.rg_ts(parsed_q, opts.include_dirs)
-  end, opts)
+  if command then
+    command(M.config, filetype)
+  else
+    print("Unknown command: " .. opts.args)
+  end
 end
 
 function M.setup(config)
   M.config = vim.tbl_deep_extend("keep", config or {}, M.config)
 
-  -- :Import
-  vim.api.nvim_create_user_command("Import", M.live_grep, {})
+  -- Commands
+  vim.api.nvim_create_user_command("Import", M.import, {
+    nargs = 1,
+    complete = function(arg_lead)
+      return vim.tbl_filter(function(val)
+        return vim.startswith(val, arg_lead)
+      end, vim.tbl_keys(commands.command))
+    end,
+  })
+
+  -- Keymap
+  for _, key in ipairs(M.config.keys) do
+    if key.enabled then
+      vim.keymap.set(
+        key.mode,
+        key.key,
+        "<cmd>Import " .. key.command .. "<CR>",
+        { desc = "fzf-lua-import " .. key.command }
+      )
+    end
+  end
+
   -- Hook into LSP
 end
 
 return M
 
 -- TODO:
---   - Register based on filetype
 --   - Hook into LSP somehow
 --   - Can we use the duplicate functionality in here and add into our code?
