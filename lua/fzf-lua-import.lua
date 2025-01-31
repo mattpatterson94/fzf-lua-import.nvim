c09f7a3f9a7c3d3f5ab7d92008a70bee6c59463c
Matt Patterson <mattpatterson@canva.com>
2025-01-31 15:24:31 +1000

    deduplicate results

--- lua/fzf-lua-import.lua
+++ lua/fzf-lua-import.lua
@@ -1,6 +1,7 @@
 local commands = require("config")
 local utils = require("utils")
 local actions = require("actions")
+local make_entry = require("fzf-lua.make_entry")
 
 local M = {
   config = {
@@ -36,6 +37,18 @@ local create_rg_glob_fn = function(filetype_opts)
   end
 end
 
+-- Filter out duplicates
+local create_fn_transform = function(opts)
+  local results = {}
+
+  return function(x)
+    if results[x] == nil then
+      results[x] = true
+      return make_entry.file(x, opts)
+    end
+  end
+end
+
 M.import = function(opts)
   local command = commands.commands[opts.args]
 
@@ -46,6 +59,7 @@ M.import = function(opts)
     if filetype_opts then
       command(vim.tbl_deep_extend("keep", {
         rg_glob_fn = create_rg_glob_fn(filetype_opts),
+        fn_transform = create_fn_transform(M.config.fzf_lua_opts),
       }, M.config.fzf_lua_opts))
     else
       return print("Unsupported filetype: " .. filetype)
